50287da00f625ed28d50d8c23a8b633822f1b470
Shubham Pandey <sp255922@gmail.com>
2025-07-10 20:15:13 +0530

    job responsive

--- app/routes/jobs/index.tsx
+++ app/routes/jobs/index.tsx
@@ -38,39 +38,39 @@ const JOBS = [
 export default function Jobs() {
     const navigate = useNavigate();
     return (
-        <div className="container mx-auto px-4 bg-white py-40 text-black">
-            <div className="text-center flex flex-col gap-14">
+        <div className="container mx-auto px-4 bg-white pt-28 pb-10 md:py-40 text-black">
+            <div className="text-center flex flex-col gap-8 md:gap-14">
                 <h1 className="text-2xl md:text-5xl font-semibold">Hey, wanna join the tribe?</h1>
-                <div className="flex gap-14 justify-center">
-                    <select className="custom-select border border-black bg-transparent font-semibold rounded-full px-4 py-2 w-2/12 text-black">
+                <div className="flex gap-2 md:gap-14 justify-center">
+                    <select className="custom-select border border-black bg-transparent font-semibold rounded-full px-4 py-2 text-xs md:text-base w-4/12 md:w-2/12 text-black">
                         <option>Location</option>
                     </select>
-                    <select className="custom-select border border-black bg-transparent font-semibold rounded-full px-4 py-2 w-2/12 text-black">
+                    <select className="custom-select border border-black bg-transparent font-semibold rounded-full px-4 py-2 text-xs md:text-base w-4/12 md:w-2/12 text-black">
                         <option>Category</option>
                     </select>
-                    <select className="custom-select border border-black bg-transparent font-semibold rounded-full px-4 py-2 w-2/12 text-black">
+                    <select className="custom-select border border-black bg-transparent font-semibold rounded-full px-4 py-2 text-xs md:text-base w-4/12 md:w-2/12 text-black">
                         <option>Experience</option>
                     </select>
                 </div>
 
-                <p className="text-xl">44 <span className="text-lightgrey">jobs in</span> all locations <span className="text-lightgrey">in</span> all categories <span className="text-lightgrey">in</span> all job types</p>
+                <p className="text-base md:text-xl">44 <span className="text-lightgrey">jobs in</span> all locations <span className="text-lightgrey">in</span> all categories <span className="text-lightgrey">in</span> all job types</p>
             </div>
 
-            <div className="flex flex-col gap-6 mt-20">
+            <div className="flex flex-col gap-4 md:gap-6 mt-10 md:mt-20">
                 {JOBS.map((job) => (
-                    <div key={job.id} className="border border-green p-8 rounded-xl cursor-pointer" onClick={() => navigate(`/jobs/${job.id}`)}>
-                        <div className="flex justify-between mb-4">
-                            <h3 className="font-semibold text-3xl">{job.label}</h3>
-                            <p className="text-xs">{job.position}</p>
+                    <div key={job.id} className="border border-green p-4 md:p-8 rounded-xl cursor-pointer" onClick={() => navigate(`/jobs/${job.id}`)}>
+                        <div className="flex justify-between mb-2 md:mb-4 gap-1">
+                            <h3 className="font-semibold text-lg md:text-3xl">{job.label}</h3>
+                            <p className="text-xs whitespace-nowrap">{job.position}</p>
                         </div>
-                        <p>{job.location.join(', ')}</p>
+                        <p className="text-sm md:text-base">{job.location.join(', ')}</p>
                     </div>
                 ))
                 }
             </div >
 
-            <div className="flex flex-col gap-6 mt-20 items-center">
-                <p className="text-xl">Showing 4 of out 42 </p>
+            <div className="flex flex-col gap-6 mt-10 md:mt-20 items-center">
+                <p className="ext-sm md:text-xl">Showing 4 of out 42 </p>
                 <button className="bg-purple border border-purple hover:bg-transparent text-white text-sm md:text-lg px-8 py-3 rounded-full hover:text-purple transition-all">
                     Load more jobs
                 </button>
